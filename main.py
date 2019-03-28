4c1993c5310fbb1bc133c58cad333acd9c2d2416
Chenjie Luo <lchenjie@tamu.edu>
2019-03-27 22:04:43 -0500

    update

--- main.py
+++ main.py
@@ -2,26 +2,17 @@ from flask import Flask, render_template, url_for
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'b581b86194d44e7069a0664a7f4313ee'
+#app.config['SECRET_KEY'] = 'b581b86194d44e7069a0664a7f4313ee'
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html')
 
-@app.route("/register")
-def register():
-    form = RegistrationForm()
-    return render_template('register.html',title='Register',form=form)
-
-@app.route("/login")
-def login():
-    form = LoginForm()
-    return render_template('login.html',title='Login',form=form)
-
 @app.route("/curriculum")
 def curriculum():
     return render_template('curriculum.html')
+
 @app.route("/education")
 def my_education():
     return render_template('education.html')
